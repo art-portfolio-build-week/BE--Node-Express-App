ea385ad0514f396818024c189ae93ee67d2351d9
Thierry Joux <tjoux@hotmail.com>
2019-06-27 11:42:41 -0700

    Changed dbConfig to work in testing environment

--- config/database/dbConfig.js
+++ config/database/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const knexConfig = require('../../knexfile.js');
 
-const environment = process.env.DB_ENV || 'development';
+const environment = process.env.DB_ENV || 'testing';
 
 module.exports = knex(knexConfig[environment]);
