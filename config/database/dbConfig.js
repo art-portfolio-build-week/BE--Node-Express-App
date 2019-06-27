02fcc8d1e72700db6be0cf534f9144012b11eeb3
Thierry Joux <tjoux@hotmail.com>
2019-06-27 13:28:56 -0700

    fixed package.json for the test script

--- config/database/dbConfig.js
+++ config/database/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const knexConfig = require('../../knexfile.js');
 
-const environment = process.env.DB_ENV || 'testing';
+const environment = process.env.DB_ENV || 'development';
 
 module.exports = knex(knexConfig[environment]);
